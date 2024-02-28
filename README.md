c446f1b3519e67860a74199f375767adbab9b36d
Nityanand Thakur <nityanandthakur252@gmail.com>
2024-02-28 09:34:01 +0530

    Updating and adding new features Changes to be committed: 	modified:   README.md 	modified:   stations.json 	modified:   train.py

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # IRCTC Train Status
-This project mainly try something new get juicy info out of Api provided by google {Whereismytrain}
-### Issues
-* stations.json is outdated. :disappointed:
+This project mainly try something new to get juicy info out of Api provided by google {Whereismytrain}
+### Updates
+* updated stations.json file to include latest stations 
+### Apis used for fetching station data
+* https://api.railyatri.in/api/common_city_station_search.json
+### Api used for train details
+
+https://whereismytrain.in/cache/live_status?train_no=**trainno**&date=**date**&lang=**en**
 
 ### Contributing
 Want to contribute to this project not limit the project to this and take it to even more heights 
--- train.py
+++ train.py
@@ -15,6 +15,10 @@ class Train:
 
         response = requests.get('https://whereismytrain.in/cache/live_status', params=params)
         return response.json()
+    def gettraindelayinfo(self):
+        for i in Train.gettrainlivestatus()["days_schedule"]:
+            if i["station_code"] == Train.gettrainlivestatus()["curStn"]:
+                return i["delay_in_arrival"]
 def main():
     tno = input('Enter the train number: ')
     date = input('Enter the date (dd-mm-yyyy) with dash or leave for today: ')
@@ -24,18 +28,33 @@ def main():
     with open('stations.json', 'r') as f:
         stations = json.load(f)
         print('Fetching the live status of train...')
-    print(f'''
-    source station : {stations[train.gettrainlivestatus()["source_station"]]} {train.gettrainlivestatus()["source_station"]}
-    current station : {stations[train.gettrainlivestatus()["curStn"]]} {train.gettrainlivestatus()["curStn"]} next stn -- {train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]} {stations[train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]]}
-    destination station : {stations[train.gettrainlivestatus()["destination_station"]]} {train.gettrainlivestatus()["destination_station"]}
-    ''')
-        # print(train.gettrainlivestatus()["curStn"])
-
-
-
+    inputno = '''
+    1. Source Station current station next station destination station
+    2. Train will come on which platform
+    3. Full json response
+            '''
+    inputnumber = input(inputno)
+    if inputnumber == '1':
+        print(f'''
+        Train Name : {train.gettrainlivestatus()["train_name"]}
+        Train Type : {train.gettrainlivestatus()["train_type"]}
+        Source Station : {stations[train.gettrainlivestatus()["source_station"]]} {train.gettrainlivestatus()["source_station"]}
+        Current Station : {stations[train.gettrainlivestatus()["curStn"]]} {train.gettrainlivestatus()["curStn"]}
+        Next Station -- {train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]} {stations[train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]]}
+        Destination Station : {stations[train.gettrainlivestatus()["destination_station"]]} {train.gettrainlivestatus()["destination_station"]}
+        ''')
+        
+        # working on Train is running late by {train.gettraindelayinfo()} minutes
+    elif inputnumber == '2':
+        print(f'enter the short code of the stations from the given list')
+        for i in train.gettrainlivestatus()["days_schedule"]:
+            print(f'{i["station_code"]} {stations[i["station_code"]]}')
+            station = input('Enter the station code: ')
+            for i in train.gettrainlivestatus()["days_schedule"]:
+                if i["station_code"] == station:
+                    print(f'Train will come on platform {i["platform"]}')
+            # print(f'Train will come on platform {train.gettrainlivestatus()["days_schedule"][station]["platform"]}')
+    elif inputnumber == '3':
+        print(train.gettrainlivestatus())
 if __name__ == "__main__":
-    # train = Train(12345, '27-02-2024', 'NDLS', 'BCT')
-    # print(train.gettrainlivestatus())
-    # print(Train.gettrainroute(12345))
-    # print(datetime.datetime.now().strftime("%d-%m-%Y"))
     main()
