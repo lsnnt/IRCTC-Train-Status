9b133f665e1bfa17d4c0e742fbf0bbeb75056db8
Nityanand Thakur <nityanandthakur252@gmail.com>
2024-02-28 11:04:26 +0530

    Changes to be committed: 	modified:   README.md 	new file:   docs/changelog/CHANGELOG 	modified:   stations.json 	modified:   train.py

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project mainly try something new to get juicy info out of Api provided by g
 * https://api.railyatri.in/api/common_city_station_search.json
 ### Api used for train details
 
-https://whereismytrain.in/cache/live_status?train_no=**trainno**&date=**date**&lang=**en**
+* https://whereismytrain.in/cache/live_status?train_no=TRAINNO&date=DATE(in dd-mm-yyyy format)&lang=hi
 
 ### Contributing
 Want to contribute to this project not limit the project to this and take it to even more heights 
--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import requests
 import datetime
 import json
-
 class Train:
     def __init__(self, train_no='12954', date=datetime.datetime.now().strftime("%d-%m-%Y")):
         self.train_no = train_no
@@ -15,10 +14,11 @@ class Train:
 
         response = requests.get('https://whereismytrain.in/cache/live_status', params=params)
         return response.json()
-    def gettraindelayinfo(self):
-        for i in Train.gettrainlivestatus()["days_schedule"]:
-            if i["station_code"] == Train.gettrainlivestatus()["curStn"]:
-                return i["delay_in_arrival"]
+def gettraindelayinfo(stations, train):
+    for i in range(0,len(train.gettrainlivestatus()["days_schedule"])):
+        if train.gettrainlivestatus()["days_schedule"][i]["station_code"] == train.gettrainlivestatus()["curStn"]:
+            return str(train.gettrainlivestatus()["days_schedule"][i-1]["delay_in_arrival"])+' minutes at station '+stations[train.gettrainlivestatus()["days_schedule"][i-1]["station_code"]]+" "+train.gettrainlivestatus()["days_schedule"][i-1]["station_code"]
+    # return 'No delay info'
 def main():
     tno = input('Enter the train number: ')
     date = input('Enter the date (dd-mm-yyyy) with dash or leave for today: ')
@@ -31,30 +31,41 @@ def main():
     inputno = '''
     1. Source Station current station next station destination station
     2. Train will come on which platform
-    3. Full json response
-            '''
+    3. Print Delay info
+    4. Full json response
+> '''
     inputnumber = input(inputno)
     if inputnumber == '1':
-        print(f'''
-        Train Name : {train.gettrainlivestatus()["train_name"]}
-        Train Type : {train.gettrainlivestatus()["train_type"]}
-        Source Station : {stations[train.gettrainlivestatus()["source_station"]]} {train.gettrainlivestatus()["source_station"]}
-        Current Station : {stations[train.gettrainlivestatus()["curStn"]]} {train.gettrainlivestatus()["curStn"]}
-        Next Station -- {train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]} {stations[train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]]}
-        Destination Station : {stations[train.gettrainlivestatus()["destination_station"]]} {train.gettrainlivestatus()["destination_station"]}
-        ''')
-        
-        # working on Train is running late by {train.gettraindelayinfo()} minutes
+        try:
+            print(f'''
+            Train Name : {train.gettrainlivestatus()["train_name"]}
+            Train Type : {train.gettrainlivestatus()["train_type"]}
+            Source Station : {stations[train.gettrainlivestatus()["source_station"]]} {train.gettrainlivestatus()["source_station"]}
+            Current Station : {stations[train.gettrainlivestatus()["curStn"]]} {train.gettrainlivestatus()["curStn"]}
+            Next Station -- {train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]} {stations[train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]]}
+            Destination Station : {stations[train.gettrainlivestatus()["destination_station"]]} {train.gettrainlivestatus()["destination_station"]}
+            ''')
+        except:
+            print(f'''
+            Train Name : {train.gettrainlivestatus()["train_name"]}
+            Train Type : {train.gettrainlivestatus()["train_type"]}
+            Source Station : {stations[train.gettrainlivestatus()["source_station"]]} {train.gettrainlivestatus()["source_station"]}
+            Current Station : {stations[train.gettrainlivestatus()["curStn"]]} {train.gettrainlivestatus()["curStn"]}
+            Next Station -- {train.gettrainlivestatus()["pitstop_next_to_curstn"]["station_code"]}
+            Destination Station : {stations[train.gettrainlivestatus()["destination_station"]]} {train.gettrainlivestatus()["destination_station"]}
+            ''')
     elif inputnumber == '2':
         print(f'enter the short code of the stations from the given list')
         for i in train.gettrainlivestatus()["days_schedule"]:
             print(f'{i["station_code"]} {stations[i["station_code"]]}')
-            station = input('Enter the station code: ')
-            for i in train.gettrainlivestatus()["days_schedule"]:
-                if i["station_code"] == station:
-                    print(f'Train will come on platform {i["platform"]}')
+        station = input('Enter the station code: ')
+        for i in train.gettrainlivestatus()["days_schedule"]:
+            if i["station_code"] == station:
+                print(f'Train will come on platform {i["platform"]}')
             # print(f'Train will come on platform {train.gettrainlivestatus()["days_schedule"][station]["platform"]}')
     elif inputnumber == '3':
+        print(gettraindelayinfo(stations, train))
+    elif inputnumber == '4':
         print(train.gettrainlivestatus())
 if __name__ == "__main__":
     main()
