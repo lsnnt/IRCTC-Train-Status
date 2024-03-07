7d4305ea0765e6e657e202ba0909991712eeedf7
lsnnt <85880458+lsnnt@users.noreply.github.com>
2024-03-07 17:02:30 +0530

    Applied some license necessary changes

--- pnr.py
+++ pnr.py
@@ -1,6 +1,24 @@
 # get pnr status
 import requests
+"""
+    Irctc-train-status
+    Copyright (C) 2024 NNT
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+    Contact developer at https://t.me/cdnnt
+"""
 # pnrno = input("input the pnr number:")
 # pnrno = 2638967619 # for testing
 def getpnrdetails(pnrno):
